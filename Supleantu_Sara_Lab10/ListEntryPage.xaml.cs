c54aea46dd5cf6a7050e31a5745f17aad53c2f3a
SARA-MARIA SUPLEANTU <sara.supleantu@stud.ubbcluj.ro>
2020-12-08 13:24:14 +0200

    Add project files.

---
+++ Supleantu_Sara_Lab10/ListEntryPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supleantu_Sara_Lab10.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Supleantu_Sara_Lab10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListEntryPage : ContentPage
+    {
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            listView.ItemsSource = await App.Database.GetShopListsAsync();
+        }
+        async void OnShopListAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListEntryPage
+            {
+                BindingContext = new ShopList()
+            });
+        }
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new ListPage
+                {
+                    BindingContext = e.SelectedItem as ShopList
+                });
+            }
+        }
+        public ListEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+
+        }
+    }
+}
